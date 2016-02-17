50646cc00cc9cbf0ea39f3be1dc106363618ad01
Simon Rawet <git@rawet.se>
2016-02-18 00:59:45 +0100

    Added minifier

---
+++ make.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#This file will create a minifed version of raw.sh
+
+#Note: !!! comment stripping is not perfect !!!
+
+
+sed 's/#.*//g' | # Removing comments
+
+#Minified
+sed '
+#Load entire file into buffer
+:a;N;$!ba;
+
+
+#Striping newlines
+s/\n\n\+/\n/g
+s/^\n*// #First
+s/\n*$// #Last
+
+s/{\n/{/g # {
+s/\n}/}/g # }
+
+
+s/ \+\n/\n/g
+s/\n/;/g
+
+
+
+#Bin sh will be stripped as a comment however it can be re introduced by uncommenting next line
+#s/^/#!\/bin\/sh\n/
+'
+
+
